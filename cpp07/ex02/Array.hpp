f6bdf7babd4ce4fceafd9a2c8b6dedd6395f2c1a
Toluvia Ruby <truby@si-k2.kzn.21-school.ru>
2021-12-02 20:43:23 +0300

    CPP07 is completed.

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -3,6 +3,56 @@
 
 # include <iostream>
 
+template<typename T>
+class Array {
+        T *array;
+        unsigned int size;
+    public:
+        Array<T>(int ArrSize = 1) {
+            int i = -1;
 
+            if (ArrSize == 0)
+                ArrSize++;
+            if (ArrSize < 0)
+                throw errorSize();
+            size = ArrSize;
+            array = new T[ArrSize];
+            while (++i < ArrSize)
+                array[i] = 0;
+        };
+
+        Array<T>(const Array &copy) {
+            this->operator=(copy);
+        };
+
+        ~Array<T>() {
+            if (size)
+                delete[] array;
+        };
+
+        Array<T> &operator = (Array<T> const &obj) {
+            int i = -1;
+
+            this->size = obj.size;
+            array = new T[this->size];
+            while (++i < this->size)
+                array[i] = obj.array[i];
+            return *this;
+        };
+        T &operator [] (int i) {
+            if (i < 0 || i >= size)
+                throw errorIndex();
+            return (array[i]);
+        };
+
+        unsigned int getSize() const {return (size);};
+
+        class errorIndex : public std::exception {
+            virtual const char *what() const throw() {return ("Error of index.\n");};
+        };
+        class errorSize : public std::exception {
+            virtual const char *what() const throw() {return ("Error of size.\n");};
+        };
+};
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,5 +1,35 @@
 #include "Array.hpp"
 
 int main() {
-    
+    /*--empty array case--*/
+    Array<int> array;
+
+    std::cout << "Empty array case: " << array.getSize() << std::endl;
+  
+    /*--full array case--*/
+    Array<int> newArray(78);
+    std::cout << "Full array case: " << newArray.getSize() << std::endl;
+
+    /*--copy constructor case--*/
+    array = newArray;
+    std::cout << "Copy constructor case: ";
+    std::cout << "newArray: " << newArray.getSize();
+    std::cout << " array: " << array.getSize() << std::endl;
+
+    /*--case with []--*/
+    std::cout << "Operator [] case: ";
+    try {std::cout << newArray[75] << std::endl;}
+    catch (std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    /*--cases with exception--*/
+    try {Array<int> wrong(-20);}
+    catch (std::exception &ex) {
+        std::cout << "Size exception case: " << ex.what();
+    }
+    try {std::cout << newArray[80] << std::endl;}
+    catch (std::exception &ex) {
+        std::cout << "Index exception case: " << ex.what();
+    }
 }
