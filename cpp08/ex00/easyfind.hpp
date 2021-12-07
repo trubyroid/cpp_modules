0a81bf37d474944b5b62b5b04cb7d673733d8837
Toluvia Ruby <truby@si-c1.kzn.21-school.ru>
2021-12-07 21:35:51 +0300

    cpp08 ex00 is completed.

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -2,10 +2,24 @@
 # define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
+
+class errorNotFound: public std::exception {
+    virtual const char *what() const throw() {return ("Error: symbol was not found.\n");};
+};
 
 template <typename T>
-typename T::iterator &easyfind(T container, int symbol) {
-    
+typename T::iterator easyfind(T &container, int symbol) {
+    if (container.begin() == container.end())
+        throw errorNotFound();
+    typename T::iterator it;
+    it = container.begin();
+    for (; it != container.end(); ++it) {
+        if (*it == symbol)
+            return (it);
+    }
+    throw errorNotFound();
 }
+
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,5 +1,70 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <set>
 
 int main() {
-    
+    std::cout << "Cases with vectors:\n";
+    std::vector<int> array;
+
+    try {std::cout << *easyfind(array, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    for (int i = 0; i < 5; i++) {
+        array.push_back(i);
+    }
+
+    try {std::cout << *easyfind(array, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    try {std::cout << *easyfind(array, 8) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    std::cout << "\nCases with lists:\n";
+    std::list<int> newList;
+
+    try {std::cout << *easyfind(newList, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    for (int i = 0; i < 10; i++) {
+        newList.push_back(i);
+    }
+
+    try {std::cout << *easyfind(newList, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+    try {std::cout << *easyfind(newList, 10) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    std::cout << "\nCases with sets:\n";
+    std::set<int> newSet;
+
+    try {std::cout << *easyfind(newSet, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+
+    for (int i = 0; i < 10; i++) {
+        newSet.insert(i);
+    }
+
+    try {std::cout << *easyfind(newSet, 4) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
+    try {std::cout << *easyfind(newList, 10) << std::endl;}
+    catch (const std::exception &ex) {
+        std::cout << ex.what();
+    }
 }
