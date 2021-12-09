cff1ba64101882dc632c45c77f60604806c89093
Toluvia Ruby <truby@et-l2.kzn.21-school.ru>
2021-12-09 19:53:44 +0300

    CPP08 is completed.

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,6 +1,29 @@
 #include "mutantstack.hpp"
 
 int main()
+// {
+//     std::list<int> mstack;
+//     mstack.push_back(5);
+//     mstack.push_back(17);
+//     std::cout << mstack.back() << std::endl;
+//     mstack.pop_back();
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push_back(3);
+//     mstack.push_back(5);
+//     mstack.push_back(737);
+//     //[...]
+//     mstack.push_back(0);
+//     std::list<int>::iterator it = mstack.begin();
+//     std::list<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     return 0;
+// }
 {
     MutantStack<int> mstack;
     mstack.push(5);
@@ -19,8 +42,8 @@ int main()
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
     std::stack<int> s(mstack);
     return 0;
---
+++ cpp08/ex02/mutantstack.hpp
@@ -0,0 +1,36 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <list>
+# include <stack>
+
+template <typename T>
+class MutantStack: public std::stack<T> {
+    public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        MutantStack() : std::stack<T> () {}
+        MutantStack(const MutantStack &copy) : std::stack<T>(copy) {}
+        ~MutantStack() {};
+
+        MutantStack &operator=(MutantStack<T> const &obj) {
+            return (std::stack<T>::operator=(obj));
+        }
+
+        iterator begin() {
+            return (this->c.begin());
+        }
+        iterator end() {
+            return (this->c.end());
+        }
+        iterator cbegin() {
+            return (this->c.cbegin());
+        }
+        iterator cend() {
+            return (this->c.cend());
+        }
+
+};
+
+#endif
